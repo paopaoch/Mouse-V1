78edbdd6c6dd7463ad1f2c37d3d23117fd3001ea
paopaoch <paopaochofficial@gmail.com>
2024-01-03 15:34:07 +0700

    add log file

--- mouse_nes.py
+++ mouse_nes.py
@@ -1,11 +1,12 @@
 import torch
 import random
-from time import time
+import time
+from datetime import datetime
+import sys
 from tqdm import tqdm
 from mouse import MMDLossFunction, NeuroNN
 from mouse_trainer_functions import get_data
 
-# TODO: Make log file
 
 def make_torch_params(mean_list, var_list, device="cpu"):
     """Return a tensor with mean and a diagonal covariance matrix, TODO: add shape checking, positive definite check"""
@@ -39,66 +40,111 @@ def sort_two_arrays(array1, array2, device="cpu"):  # sort according to array1
     return torch.tensor(sorted_array1, device=device), torch.stack(sorted_array2)
 
 
-def nes_multigaussian_optim(mean, cov, max_iter, samples_per_iter, Y, eta_delta=0.001, eta_sigma=0.001, eta_B=0.001, device="cpu"):
-    
+def nes_multigaussian_optim(mean, cov, max_iter, samples_per_iter, Y, neuron_num=10000, eta_delta=0.01, eta_sigma=0.001, eta_B=0.001, device="cpu", avg_step_weighting=0.002, desc=""):
     # Init model and loss function
     J, P, w = mean_to_params(mean)
-    model = NeuroNN(J, P, w, 10000, device=device, grad=False)
-    loss_function = MMDLossFunction(device=device)
+    model = NeuroNN(J, P, w, neuron_num, device=device, grad=False)
+    loss_function = MMDLossFunction(device=device, avg_step_weighting=avg_step_weighting)
 
+    with open(f"log_nes_run_{time.time()}.log", "w") as f:
+        # write the metadata to log file
+        f.write("#### Mouse V1 Project log file ####\n\n")
+        f.write(f"Code ran on the {datetime.now()}\n\n")
+        f.write(f"Device: {device}\n")
+        f.write(f"OS: {sys.platform}\n")
+        f.write("Trainer type: xNES\n\n")
+        f.write(f"{desc}\n\n")
+        f.write("Metadata:\n")
+        f.write(f"Number of neurons: {model.neuron_num}\n")
+        f.write(f"Number of Euler steps: {model.Nmax}\n")
+        f.write(f"Record average step after {model.Navg} steps\n")
+        f.write(f"Average step weighting: {avg_step_weighting}\n")
+        f.write(f"Number of xNES optimisation step: {max_iter}\n")
+        f.write(f"Number of of samples per optimisation step: {samples_per_iter}\n")
+        f.write(f"Learning Rates: eta_delta={eta_delta}, eta_sigma={eta_sigma}, eta_B={eta_B}\n")
+        f.write(f"---------------------------------------------------\n\n")
+        f.write(f"Initial parameters\n")
+        f.write(f"J\n")
+        f.write(str(model.j_hyperparameter))
+        f.write("\n")
+        f.write(f"P\n")
+        f.write(str(model.p_hyperparameter))
+        f.write("\n")
+        f.write(f"w\n")
+        f.write(str(model.w_hyperparameter))
+        f.write("\n\n")
+        f.write("Covariance Matrix\n")
+        f.write(str(cov))
+        f.write("\n\n")
+        f.write(f"---------------------------------------------------\n\n\n")
+        f.flush()
 
-    d = len(mean)  # dimensions
-    # Get cov = A^TA
-    A_T: torch.Tensor = torch.linalg.cholesky(cov)
-    A = A_T.t()
+        d = len(mean)  # dimensions
+        # Get cov = A^TA
+        A_T: torch.Tensor = torch.linalg.cholesky(cov)
+        A = A_T.t()
 
-    # Get sigma
-    sigma = torch.pow(torch.det(A), 1/d)
-    B = A / sigma
+        # Get sigma
+        sigma = torch.pow(torch.det(A), 1/d)
+        B = A / sigma
 
-    # initialise a multigaussian dist for sample
-    mean_zeros = torch.zeros(d, device=device)
-    cov_iden = torch.eye(d, device=device)
-    multivariate_normal = torch.distributions.MultivariateNormal(mean_zeros, cov_iden)
+        # initialise a multigaussian dist for sample
+        mean_zeros = torch.zeros(d, device=device)
+        cov_iden = torch.eye(d, device=device)
+        multivariate_normal = torch.distributions.MultivariateNormal(mean_zeros, cov_iden)
 
-    for _ in tqdm(range(max_iter)):
-        samples = multivariate_normal.sample((samples_per_iter,))
-        samples.to(device=device)
-        losses = []
-        J, P, w = mean_to_params(mean)
-        preds, avg_step = model()
-        mean_loss, mean_MMD_loss = loss_function(preds, Y, avg_step)
-        print("current_loss: ", mean_loss, mean_MMD_loss)
-        print("mean: ", mean)
-        for k in range(samples_per_iter):
-            zk = mean + sigma * (B.t() @ samples[k])
-
-            J, P, w = mean_to_params(zk)
-            model.set_parameters(J, P, w)
+        for i in tqdm(range(max_iter)):
+            f.write(f"ITERATION: {i}\n")
+            samples = multivariate_normal.sample((samples_per_iter,))
+            samples.to(device=device)
+            losses = []
+            J, P, w = mean_to_params(mean)
             preds, avg_step = model()
-            current_loss, MMD_loss = loss_function(preds, Y, avg_step)
-            losses.append(current_loss.clone().detach())
+            mean_loss, mean_MMD_loss = loss_function(preds, Y, avg_step)
+            print("current_loss: ", mean_loss, mean_MMD_loss)
+            print("mean: ", mean)
+            f.write(f"Mean: {mean}\n")
+            f.write(f"Mean loss: {mean_loss}\n")
+            for k in range(samples_per_iter):
+                zk = mean + sigma * (B.t() @ samples[k])
 
-        loss_sorted, samples_sorted = sort_two_arrays(losses, samples, device=device)
-        utilities = get_utilities(loss_sorted, device=device)
+                J, P, w = mean_to_params(zk)
+                model.set_parameters(J, P, w)
+                preds, avg_step = model()
+                current_loss, MMD_loss = loss_function(preds, Y, avg_step)
+                losses.append(current_loss.clone().detach())
 
-        # Compute gradients
-        grad_delta = (samples_sorted.permute(1, 0) * utilities).permute(1, 0).sum(dim=(0))
-        grad_M = torch.zeros(size=(len(samples_sorted[0]), len(samples_sorted[0])), device=device)  # does not make sense to condense this
-        for k, sample in enumerate(samples_sorted):
-            grad_M += ((sample * sample.t()) - torch.eye(len(sample), device=device)) * utilities[k]
+            loss_sorted, samples_sorted = sort_two_arrays(losses, samples, device=device)
+            utilities = get_utilities(loss_sorted, device=device)
+            
+            avg_loss = torch.mean(loss_sorted)
+            print("Avg loss", avg_loss)
+            f.write(f"Avg loss {avg_loss}\n")
+            f.write("\n\n\n")
 
-        grad_sigma = torch.trace(grad_M) / d
-        grad_B = torch.trace(grad_M) - grad_sigma * torch.eye(len(grad_M), device=device)
+            # Compute gradients
+            grad_delta = (samples_sorted.permute(1, 0) * utilities).permute(1, 0).sum(dim=(0))
+            grad_M = torch.zeros(size=(len(samples_sorted[0]), len(samples_sorted[0])), device=device)  # does not make sense to condense this
+            for k, sample in enumerate(samples_sorted):
+                grad_M += ((sample * sample.t()) - torch.eye(len(sample), device=device)) * utilities[k]
 
-        # Update parameters
-        mean = mean + eta_delta * grad_delta
-        sigma = sigma * torch.exp((eta_sigma / 2) * grad_sigma)
-        B = B * torch.exp((eta_B / 2) * grad_B)
+            grad_sigma = torch.trace(grad_M) / d
+            grad_B = torch.trace(grad_M) - grad_sigma * torch.eye(len(grad_M), device=device)
 
-    # Get back parameters
-    A_optimised = B * sigma
-    cov_optimised = A_optimised.t() @ A_optimised
+            # Update parameters
+            mean = mean + eta_delta * grad_delta
+            sigma = sigma * torch.exp((eta_sigma / 2) * grad_sigma)
+            B = B * torch.exp((eta_B / 2) * grad_B)
+            f.flush()
+
+        # Get back parameters
+        A_optimised = B * sigma
+        cov_optimised = A_optimised.t() @ A_optimised
+        
+        f.write("Final covariance matrix:\n")
+        f.write(cov_optimised)
+        f.write("\n\n")
+        f.flush()
 
     return mean, cov_optimised
 
@@ -119,10 +165,10 @@ if __name__ == "__main__":
     
     var_list = [5, 5, 5, 5, 
                 5, 5, 5, 5, 
-                1, 1, 1, 1]
+                5, 5, 5, 5]
     
     mean, cov = make_torch_params(mean_list, var_list, device=device)
 
     Y = get_data(device=device)
 
-    print(nes_multigaussian_optim(mean, cov, 15, 36, Y, device=device))  # TODO: need to implement xNES important sampling
+    print(nes_multigaussian_optim(mean, cov, 1, 12, Y, device=device))  # TODO: need to implement xNES important sampling
