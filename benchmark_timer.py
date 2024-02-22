f2ddf1ee7fe27989fd4f0f46f0b832b62d1f62a4
paopaoch <paopaochofficial@gmail.com>
2024-02-22 13:53:57 +0000

    10000 neurons

--- benchmark_timer.py
+++ benchmark_timer.py
@@ -10,13 +10,15 @@ else:
     print("GPU not available. Model will be created on CPU.")
 
 X, Y = get_data(device=device)
+n = 10000
+n_E = 8000
 
 J_array = [-13.862943611198906, -24.423470353692043, -6.1903920840622355, -24.423470353692043, -13.862943611198906, -13.862943611198906]
 P_array = [-2.5418935811616112, 4.1588830833596715, -2.5418935811616112, 4.1588830833596715, -2.5418935811616112, -2.5418935811616112]
 w_array = [-237.28336161747745, -255.84942256760897, -214.12513203729057, -225.49733432916625, -237.28336161747745, -237.28336161747745] 
 
-WG = WeightsGenerator(J_array, P_array, w_array, 4000, device=device)
-NE = NetworkExecuter(4000, device=device)
+WG = WeightsGenerator(J_array, P_array, w_array, n, device=device)
+NE = NetworkExecuter(n, device=device)
 loss_function = MouseLossFunction(device=device)
 
 start = time.time()
@@ -29,5 +31,5 @@ response, avg_step = NE.run_all_orientation_and_contrast(weights=recurrent_weigh
 print("Get all tuning curves took:", time.time() - start)
 
 start = time.time()
-loss = loss_function.calculate_loss(response[:3200], X, response[3200:], Y, avg_step)
+loss = loss_function.calculate_loss(response[:n_E], X, response[n_E:], Y, avg_step)
 print("Calculating loss took:", time.time() - start)
