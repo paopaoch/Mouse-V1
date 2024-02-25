87843c6880ce34f2c3cc94807e7afe2bb82bba71
paopaoch <paopaochofficial@gmail.com>
2024-02-25 23:50:24 +0000

    New params

--- method_validation.py
+++ method_validation.py
@@ -4,7 +4,7 @@ import pickle
 
 
 if __name__ == "__main__":
-    desc = "Removing important mixing seems to work but we will try to increase the learning rate as well as the covariance matrix size for a faster convergence."
+    desc = "To validate our method, we will try to optimise the network but with known parameters."
 
     if torch.cuda.is_available():
         device = "cuda:0"
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         
     mean_list = [-8.472978603872036, -16.582280766035325, 13.862943611198906, -21.972245773362197, -1.2163953243244932, -0.0, -4.1588830833596715, 4.1588830833596715, 40.165839236557744, -40.16583923655776, 81.35732227375028, 60.56500259181835] 
      
-    var_list = [0.1, 0.1, 0.1, 0.1, 
-                0.1, 0.1, 0.1, 0.1, 
-                0.1, 0.1, 0.1, 0.1]
+    var_list = [0.01, 0.01, 0.01, 0.01, 
+                0.01, 0.01, 0.01, 0.01, 
+                0.01, 0.01, 0.01, 0.01]
     
     mean, cov = make_torch_params(mean_list, var_list, device=device)
     
@@ -28,4 +28,4 @@ if __name__ == "__main__":
         responses = 0
         print(y_E.shape, y_I.shape)
 
-    print(nes_multigaussian_optim(mean, cov, 200, 24, y_E, y_I, device=device, neuron_num=1000, desc=desc, trials=3, alpha=1, eta_delta=1, avg_step_weighting=0.0001))
+    print(nes_multigaussian_optim(mean, cov, 200, 12, y_E, y_I, device=device, neuron_num=1000, desc=desc, trials=1, alpha=1, eta_delta=1, avg_step_weighting=0.0001))
