05dc6a02b60992c8e6d6c4b11a781ddd7a8c9157
paopaoch <paopaochofficial@gmail.com>
2024-03-20 03:22:12 +0000

    Change params

--- mouse_nes_finding_lambda.py
+++ mouse_nes_finding_lambda.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     desc = "For finding lambda"
 
     if torch.cuda.is_available():
-        device = "cuda:1"
+        device = "cuda:0"
         print("Model will be created on GPU")
     else:
         device = "cpu"
@@ -26,8 +26,10 @@ if __name__ == "__main__":
 
     y_E, y_I = get_data(device=device)
     
-    # lambdas = [12, 24, 36, 48, 60]
-    lambdas = [18, 30, 42, 54, 66]
+    lambdas = [12, 66]
+    # lambdas = [18, 60]
+    # lambdas = [24, 36, 54]
+    # lambdas = [30, 42, 48]
     
     for lambda_ in lambdas:
         for _ in range(4):
