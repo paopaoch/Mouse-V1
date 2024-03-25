88726b3399e8687e8522f8c7307e363bfff55df0
paopaoch <paopaochofficial@gmail.com>
2024-03-25 18:36:04 +0100

    Change condition

--- mouse_gd_backwards.py
+++ mouse_gd_backwards.py
@@ -58,6 +58,7 @@ for i in range(200):
     print("\n\n")
 
     loss_diffs.append(trial_loss.clone().detach() - prev_loss)
+    print(torch.tensor(loss_diffs[-10:], device=device).mean())
     if i > 30 and torch.tensor(loss_diffs[-10:], device=device).mean() < 1e-10: # This is the same stopping criterion as xNES which could be appropriate but the learning rate is different.
         print("Early stopping")
         break
