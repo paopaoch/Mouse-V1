f52a4876a92819f87e9cce8c258f2e2956dbd4ff
paopaoch <paopaochofficial@gmail.com>
2024-03-25 18:06:51 +0100

    add stopping criterion

--- mouse_gd_backwards.py
+++ mouse_gd_backwards.py
@@ -1,6 +1,9 @@
 import torch
 import pickle
 from rat import MouseLossFunctionOptimised, WeightsGenerator, NetworkExecuterParallel, get_data
+from time import time
+
+start = time()
 
 if torch.cuda.is_available():
     device = "cuda:0"
@@ -26,6 +29,8 @@ w_array = torch.tensor(w_array, device= device, requires_grad=True)
 executer = NetworkExecuterParallel(1000, device=device)
 loss_function = MouseLossFunctionOptimised(device=device)
 
+
+losses = []
 for i in range(100):
     wg = WeightsGenerator(J_array, P_array, w_array, 1000, device=device, forward_mode=True)
 
@@ -50,5 +55,10 @@ for i in range(100):
 
     print("\n\n")
 
+    losses.append(trial_loss.clone().detach())
+    if i > 20 and torch.tensor(losses[-10:], device=device) < 1e-5: # This is the same stopping criterion as xNES which could be appropriate but the learning rate is different.
+        print("Early stopping")
+        break
 
-    
+end = time()
+print(f"Time Taken: {end - start}")
