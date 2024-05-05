db94efdccf6a54edb101e9d4db1a73caa793d0c4
paopaoch <paopaochofficial@gmail.com>
2024-05-05 01:50:05 +0100

    change n

--- method_validation.py
+++ method_validation.py
@@ -12,23 +12,22 @@ if __name__ == "__main__":
         device = "cpu"
         print("GPU not available. Model will be created on CPU.")
 
-
-    # Create dataset
-    J_array = [-1.411484609948449, -2.4642873625852646, -0.8765172970974294, -2.2196470413920517]
+    J_array = [-1.6582280766035324, -2.68273239311792, -1.1526795099383855, -2.4423470353692043]  # n = 2000
     P_array = [-2.0907410969337694, -0.20067069546215124, -2.0907410969337694, -0.20067069546215124]
     w_array = [-1.5314763709643886, -1.5314763709643886, -1.5314763709643886, -1.5314763709643886] 
 
     J_array = torch.tensor(J_array, device= device, requires_grad=True)
     P_array = torch.tensor(P_array, device= device, requires_grad=True)
     w_array = torch.tensor(w_array, device= device, requires_grad=True)
-    executer = NetworkExecuterParallel(3000, device=device)
+    executer = NetworkExecuterParallel(2000, device=device)
     loss_function = MouseLossFunctionOptimised(device=device)
 
+
     # Create dataset
-    wg = WeightsGenerator(J_array, P_array, w_array, 3000, device=device, forward_mode=True)
+    wg = WeightsGenerator(J_array, P_array, w_array, 2000, device=device, forward_mode=True)
     W = wg.generate_weight_matrix()
     tuning_curves, avg_step = executer.run_all_orientation_and_contrast(W)
-    y_E, y_I = tuning_curves[:2400], tuning_curves[2400:]
+    y_E, y_I = tuning_curves[:1600], tuning_curves[1600:]
 
 
     loss_diffs = []
@@ -36,11 +35,11 @@ if __name__ == "__main__":
     stopping_criterion_count = 0
     for i in range(200):
         print(i)
-        wg = WeightsGenerator(J_array, P_array, w_array, 3000, device=device, forward_mode=True)
+        wg = WeightsGenerator(J_array, P_array, w_array, 2000, device=device, forward_mode=True)
 
         W = wg.generate_weight_matrix()
         tuning_curves, avg_step = executer.run_all_orientation_and_contrast(W)
-        x_E, x_I = tuning_curves[:2400], tuning_curves[2400:]
+        x_E, x_I = tuning_curves[:1600], tuning_curves[1600:]
         bessel_val = wg.validate_weight_matrix()
 
         print("bessel_val:", bessel_val)
@@ -63,7 +62,7 @@ if __name__ == "__main__":
         print("loss_diff", torch.tensor(loss_diffs[-10:], device=device).mean())
         print("\n\n")
 
-        if i > 30 and torch.tensor(loss_diffs[-10:], device=device).mean() < 1e-5: # This is the same stopping criterion as xNES which could be appropriate but the learning rate is different.
+        if i > 40 and torch.tensor(loss_diffs[-10:], device=device).mean() < 1e-5: # This is the same stopping criterion as xNES which could be appropriate but the learning rate is different.
             print("Early stopping")
             if stopping_criterion_count >= 2:
                 break
--- parameter_tester.py
+++ parameter_tester.py
@@ -332,13 +332,17 @@ if __name__ == "__main__":
 
     SEARCH_INIT = bool(input("Search Params? (default False): "))
 
+    N_E = 1600
+    N_I = 400
+    N_F = 1000
+
     # N_E = 2400
     # N_I = 600
     # N_F = 1000
 
-    N_E = 8000
-    N_I = 2000
-    N_F = 1000
+    # N_E = 8000
+    # N_I = 2000
+    # N_F = 1000
 
     if SEARCH_INIT:
         EE, EI, IE, II = get_random_valid_params(trials=1000, n=N_E + N_I)
@@ -351,20 +355,25 @@ if __name__ == "__main__":
         print("\nInitial: ", INITIAL, '\n')
 
         if INITIAL:
-            EE = Connection(35.78, 0.11, 32, N_E)  # 10000
-            EI = Connection(14.31, 0.45, 32, N_I)
-            IE = Connection(53.67, 0.11, 32, N_E)
-            II = Connection(17.89, 0.45, 32, N_I)
+            # EE = Connection(35.78, 0.11, 32, N_E)  # 10000
+            # EI = Connection(14.31, 0.45, 32, N_I)
+            # IE = Connection(53.67, 0.11, 32, N_E)
+            # II = Connection(17.89, 0.45, 32, N_I)
 
             # EE = Connection(19.60, 0.11, 32, N_E)  # 3000
             # EI = Connection(7.84, 0.45, 32, N_I)
             # IE = Connection(29.39, 0.11, 32, N_E)
             # II = Connection(9.80, 0.45, 32, N_I)
 
-            EF = Connection(5, 0.11, 30, N_F)
-            IF = Connection(5, 0.11, 30, N_F)
-            # EF = None
-            # IF = None
+            EE = Connection(16, 0.11, 32, N_E)  # 2000
+            EI = Connection(6.4, 0.45, 32, N_I)
+            IE = Connection(24, 0.11, 32, N_E)
+            II = Connection(8, 0.45, 32, N_I)
+
+            # EF = Connection(5, 0.11, 30, N_F)
+            # IF = Connection(5, 0.11, 30, N_F)
+            EF = None
+            IF = None
         else:
             mean_list = [-23.1309, -38.8711, -11.8566, -26.8844, -3.2984, 10.7771, -2.4424, 10.8624, -138.8649, -140.7406, -122.6129, -125.8241]
 
