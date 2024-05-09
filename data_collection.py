c99b410cdacd301897deb4b96157ee0662a2a05c
paopaoch <paopaochofficial@gmail.com>
2024-05-09 23:46:32 +0100

    add results

--- data_collection.py
+++ data_collection.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 N = 1000
 E_index = 800
-device = get_device("cuda:0")
+device = get_device("cuda:1")
 executer = NetworkExecuterParallel(N, device=device)
 loss_func = MouseLossFunctionOptimised(device=device)
 
