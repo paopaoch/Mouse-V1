144a3cbfc6e13de7524b891519152b5b1cc6197d
paopaoch <paopaochofficial@gmail.com>
2024-05-14 01:35:02 +0100

    change temp

--- temp.py
+++ temp.py
@@ -11,6 +11,11 @@ for datum in data:
     temp_datum["J_predicted"] = datum["J_predicted"].clone().detach().to("cpu")
     temp_datum["P_predicted"] = datum["P_predicted"].clone().detach().to("cpu")
     temp_datum["w_predicted"] = datum["w_predicted"].clone().detach().to("cpu")
+
+    temp_datum["J_array"] = datum["J_array"].clone().detach().to("cpu")
+    temp_datum["P_array"] = datum["P_array"].clone().detach().to("cpu")
+    temp_datum["w_array"] = datum["w_array"].clone().detach().to("cpu")
+
     temp_datum["loss"] = datum["loss"].clone().detach().to("cpu")
     results.append(temp_datum)
 
