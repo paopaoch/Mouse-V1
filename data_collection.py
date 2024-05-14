ff4cb0537de6562958ecea5c098066fdc058e3c2
paopaoch <paopaochofficial@gmail.com>
2024-05-14 04:28:57 +0100

    add contrast and orientation pairs

--- data_collection.py
+++ data_collection.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     executer = NetworkExecuterParallel(N, device=device)
     executer.contrasts = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
     executer.orientations = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170]
+    executer.__init__(N, device=device)
     loss_func = MouseLossFunctionOptimised(device=device)
 
     config13 = {
