4ae9fef8b8d3b3b035959cd572351baf6534acba
paopaoch <paopaochofficial@gmail.com>
2024-05-15 15:34:20 +0100

    change device

--- data_collection.py
+++ data_collection.py
@@ -9,7 +9,7 @@ from tqdm import tqdm
 if __name__ == "__main__":
     N = 1000
     E_index = 800
-    device = get_device("cuda:1")
+    device = get_device("cuda:0")
     executer = NetworkExecuterParallel(N, device=device, contrasts=[0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
                                        , orientations=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170])
     loss_func = MouseLossFunctionOptimised(device=device)
