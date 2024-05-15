3007ffc4faba59a0e17f36ad8417ee70d8451b99
paopaoch <paopaochofficial@gmail.com>
2024-05-15 15:15:53 +0100

    add new CNN file

--- data_collection.py
+++ data_collection.py
@@ -13,134 +13,137 @@ if __name__ == "__main__":
     executer = NetworkExecuterParallel(N, device=device)
     executer.contrasts = [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1]
     executer.orientations = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170]
-    executer.__init__(N, device=device)
-    loss_func = MouseLossFunctionOptimised(device=device)
 
-    config13 = {
-        "J" : [-2.059459853260332, -3.0504048076264896, -1.5877549090278045, -2.813481385641024],
-        "P" : [-2.0907410969337694, -0.20067069546215124, -2.0907410969337694, -0.20067069546215124],
-        "w" : [-1.5314763709643886, -1.5314763709643886, -1.5314763709643886, -1.5314763709643886]
-    }
+    print(len(executer.contrasts))
+    print(len(executer.orientations))
+#     executer.__init__(N, device=device)
+#     loss_func = MouseLossFunctionOptimised(device=device)
 
-    WG13 = WeightsGenerator(config13["J"], config13["P"], config13["w"], N, device=device)
-    W = WG13.generate_weight_matrix()
-    tuning_curves, _ = executer.run_all_orientation_and_contrast(W)
-    config_E, config_I = tuning_curves[:E_index], tuning_curves[E_index:]
+#     config13 = {
+#         "J" : [-2.059459853260332, -3.0504048076264896, -1.5877549090278045, -2.813481385641024],
+#         "P" : [-2.0907410969337694, -0.20067069546215124, -2.0907410969337694, -0.20067069546215124],
+#         "w" : [-1.5314763709643886, -1.5314763709643886, -1.5314763709643886, -1.5314763709643886]
+#     }
 
-    dir_name = f"DATASET_bessel_large_{time.time()}"
-    create_directory_if_not_exists(dir_name)
-    metadata_file = f"{dir_name}/metadata.csv"
-    with open(metadata_file, 'w') as f:
-        f.write('dir,J_EE,J_EI,J_IE,J_II,P_EE,P_EI,P_IE,P_II,w_EE,w_EI,w_IE,w_II')
+#     WG13 = WeightsGenerator(config13["J"], config13["P"], config13["w"], N, device=device)
+#     W = WG13.generate_weight_matrix()
+#     tuning_curves, _ = executer.run_all_orientation_and_contrast(W)
+#     config_E, config_I = tuning_curves[:E_index], tuning_curves[E_index:]
 
-    def execute_network(W):
-        tuning_curves, avg_step = executer.run_all_orientation_and_contrast(W)
-        y_E, y_I = tuning_curves[:E_index], tuning_curves[E_index:]
-        return y_E, y_I, avg_step
+#     dir_name = f"DATASET_bessel_large_{time.time()}"
+#     create_directory_if_not_exists(dir_name)
+#     metadata_file = f"{dir_name}/metadata.csv"
+#     with open(metadata_file, 'w') as f:
+#         f.write('dir,J_EE,J_EI,J_IE,J_II,P_EE,P_EI,P_IE,P_II,w_EE,w_EI,w_IE,w_II')
 
-
-def get_random_param():
-    J_array = np.random.rand(4) * 9 - 4.5
-    P_array = np.random.rand(4) * 9 - 4.5
-    w_array = np.random.rand(4) * 9 - 4.5
-    J_array = torch.tensor(J_array, device=device, requires_grad=True)
-    P_array = torch.tensor(P_array, device=device, requires_grad=True)
-    w_array = torch.tensor(w_array, device=device, requires_grad=True)
-    return J_array, P_array, w_array
+#     def execute_network(W):
+#         tuning_curves, avg_step = executer.run_all_orientation_and_contrast(W)
+#         y_E, y_I = tuning_curves[:E_index], tuning_curves[E_index:]
+#         return y_E, y_I, avg_step
 
 
-def run_gd(J_array, P_array, w_array, y_E, y_I, iterations=10, search_bessel=True):
-    valid_count = 0
-    found = False
-    for _ in tqdm(range(iterations)):
-        wg = WeightsGenerator(J_array, P_array, w_array, N, device=device, forward_mode=True)
-        W = wg.generate_weight_matrix()
-        bessel_val = wg.validate_weight_matrix()
-        # x_E, x_I, avg_step = execute_network(W)
-        # loss, _ = loss_func.calculate_loss(x_E, x_I, y_E, y_I, avg_step, bessel_val)
-        loss = bessel_val
-        loss.backward()
-        J_array: torch.Tensor = (J_array - 1 * wg.J_parameters.grad).clone().detach().requires_grad_(True)
-        P_array: torch.Tensor = (P_array - 1 * wg.P_parameters.grad).clone().detach().requires_grad_(True)
-        w_array: torch.Tensor = (w_array - 1 * wg.w_parameters.grad).clone().detach().requires_grad_(True)
+# def get_random_param():
+#     J_array = np.random.rand(4) * 9 - 4.5
+#     P_array = np.random.rand(4) * 9 - 4.5
+#     w_array = np.random.rand(4) * 9 - 4.5
+#     J_array = torch.tensor(J_array, device=device, requires_grad=True)
+#     P_array = torch.tensor(P_array, device=device, requires_grad=True)
+#     w_array = torch.tensor(w_array, device=device, requires_grad=True)
+#     return J_array, P_array, w_array
+
+
+# def run_gd(J_array, P_array, w_array, y_E, y_I, iterations=10, search_bessel=True):
+#     valid_count = 0
+#     found = False
+#     for _ in tqdm(range(iterations)):
+#         wg = WeightsGenerator(J_array, P_array, w_array, N, device=device, forward_mode=True)
+#         W = wg.generate_weight_matrix()
+#         bessel_val = wg.validate_weight_matrix()
+#         # x_E, x_I, avg_step = execute_network(W)
+#         # loss, _ = loss_func.calculate_loss(x_E, x_I, y_E, y_I, avg_step, bessel_val)
+#         loss = bessel_val
+#         loss.backward()
+#         J_array: torch.Tensor = (J_array - 1 * wg.J_parameters.grad).clone().detach().requires_grad_(True)
+#         P_array: torch.Tensor = (P_array - 1 * wg.P_parameters.grad).clone().detach().requires_grad_(True)
+#         w_array: torch.Tensor = (w_array - 1 * wg.w_parameters.grad).clone().detach().requires_grad_(True)
         
-        if torch.isnan(J_array).any().item() or torch.isnan(P_array).any().item() or torch.isnan(w_array).any().item():
-            found = False
-            break
+#         if torch.isnan(J_array).any().item() or torch.isnan(P_array).any().item() or torch.isnan(w_array).any().item():
+#             found = False
+#             break
 
-        if search_bessel:
-            if bessel_val == 0:
-                valid_count += 1
-            if valid_count == 4:
-                found = True
-                break
+#         if search_bessel:
+#             if bessel_val == 0:
+#                 valid_count += 1
+#             if valid_count == 4:
+#                 found = True
+#                 break
 
-    return J_array, P_array, w_array, found
-
-
-def get_valid_params():
-    J_array, P_array, w_array = get_random_param()
-    return run_gd(J_array, P_array, w_array, config_E, config_I)
+#     return J_array, P_array, w_array, found
 
 
-def random_sample_from_tensor(input_tensor, x):
-    total_elements = input_tensor.size(0)
-    random_indices = torch.randperm(total_elements)[:x]
-    sampled_tensor = input_tensor[random_indices]
-    return sampled_tensor
+# def get_valid_params():
+#     J_array, P_array, w_array = get_random_param()
+#     return run_gd(J_array, P_array, w_array, config_E, config_I)
 
 
-def trim_data(y_E, y_I, size=60):
-    total = len(y_E) + len(y_I)
-    size_E = int((size * len(y_E) / total))
-    size_I = size - size_E
-    reduced_E = random_sample_from_tensor(y_E, size_E)
-    reduced_I = random_sample_from_tensor(y_I, size_I)
-    return reduced_E, reduced_I
+# def random_sample_from_tensor(input_tensor, x):
+#     total_elements = input_tensor.size(0)
+#     random_indices = torch.randperm(total_elements)[:x]
+#     sampled_tensor = input_tensor[random_indices]
+#     return sampled_tensor
 
 
-def save_data(y_E, y_I, J_array, P_array, w_array):
-    sub_dir_name = f"{time.time()}"
-    full_sub_dir_name = f"{dir_name}/{sub_dir_name}"
-    create_directory_if_not_exists(full_sub_dir_name)
-    with open(f"{full_sub_dir_name}/y_E.pkl", "wb") as f:
-        pickle.dump(y_E, f)
-    with open(f"{full_sub_dir_name}/y_I.pkl", "wb") as f:
-        pickle.dump(y_I, f)
-
-    J_array = round_1D_tensor_to_list(J_array)
-    P_array = round_1D_tensor_to_list(P_array)
-    w_array = round_1D_tensor_to_list(w_array)
-
-    with open(metadata_file, 'a') as f:
-        f.write(f"\n{sub_dir_name}"
-                + f",{round(params_to_J_scalar(J_array[0]), 4)},{round(params_to_J_scalar(J_array[1]), 4)},{round(params_to_J_scalar(J_array[2]), 4)},{round(params_to_J_scalar(J_array[3]), 4)}"
-                + f",{round(params_to_P_scalar(P_array[0]), 4)},{round(params_to_P_scalar(P_array[1]), 4)},{round(params_to_P_scalar(P_array[2]), 4)},{round(params_to_P_scalar(P_array[3]), 4)}"
-                + f",{round(params_to_w_scalar(w_array[0]), 4)},{round(params_to_w_scalar(w_array[1]), 4)},{round(params_to_w_scalar(w_array[2]), 4)},{round(params_to_w_scalar(w_array[3]), 4)}")
+# def trim_data(y_E, y_I, size=60):
+#     total = len(y_E) + len(y_I)
+#     size_E = int((size * len(y_E) / total))
+#     size_I = size - size_E
+#     reduced_E = random_sample_from_tensor(y_E, size_E)
+#     reduced_I = random_sample_from_tensor(y_I, size_I)
+#     return reduced_E, reduced_I
 
 
-def main(dataset_size=3000):
-    count = 0
-    i = 0
-    while count <= dataset_size:
-        i += 1
+# def save_data(y_E, y_I, J_array, P_array, w_array):
+#     sub_dir_name = f"{time.time()}"
+#     full_sub_dir_name = f"{dir_name}/{sub_dir_name}"
+#     create_directory_if_not_exists(full_sub_dir_name)
+#     with open(f"{full_sub_dir_name}/y_E.pkl", "wb") as f:
+#         pickle.dump(y_E, f)
+#     with open(f"{full_sub_dir_name}/y_I.pkl", "wb") as f:
+#         pickle.dump(y_I, f)
+
+#     J_array = round_1D_tensor_to_list(J_array)
+#     P_array = round_1D_tensor_to_list(P_array)
+#     w_array = round_1D_tensor_to_list(w_array)
+
+#     with open(metadata_file, 'a') as f:
+#         f.write(f"\n{sub_dir_name}"
+#                 + f",{round(params_to_J_scalar(J_array[0]), 4)},{round(params_to_J_scalar(J_array[1]), 4)},{round(params_to_J_scalar(J_array[2]), 4)},{round(params_to_J_scalar(J_array[3]), 4)}"
+#                 + f",{round(params_to_P_scalar(P_array[0]), 4)},{round(params_to_P_scalar(P_array[1]), 4)},{round(params_to_P_scalar(P_array[2]), 4)},{round(params_to_P_scalar(P_array[3]), 4)}"
+#                 + f",{round(params_to_w_scalar(w_array[0]), 4)},{round(params_to_w_scalar(w_array[1]), 4)},{round(params_to_w_scalar(w_array[2]), 4)},{round(params_to_w_scalar(w_array[3]), 4)}")
+
+
+# def main(dataset_size=3000):
+#     count = 0
+#     i = 0
+#     while count <= dataset_size:
+#         i += 1
         
-        if i > 10000:
-            raise BufferError(f"Tried for too many iterations generated {count} rows")
+#         if i > 10000:
+#             raise BufferError(f"Tried for too many iterations generated {count} rows")
 
-        J_array, P_array, w_array, found = get_valid_params()
-        J_array = J_array.requires_grad_(False)
-        P_array = P_array.requires_grad_(False)
-        w_array = w_array.requires_grad_(False)
-        if not found:
-            continue
-        wg = WeightsGenerator(J_array, P_array, w_array, N, device=device)
-        W = wg.generate_weight_matrix()
-        y_E, y_I, _ = execute_network(W)
-        y_E, y_I = trim_data(y_E, y_I)
-        save_data(y_E, y_I, J_array, P_array, w_array)
-        count += 1
-        print(count)
+#         J_array, P_array, w_array, found = get_valid_params()
+#         J_array = J_array.requires_grad_(False)
+#         P_array = P_array.requires_grad_(False)
+#         w_array = w_array.requires_grad_(False)
+#         if not found:
+#             continue
+#         wg = WeightsGenerator(J_array, P_array, w_array, N, device=device)
+#         W = wg.generate_weight_matrix()
+#         y_E, y_I, _ = execute_network(W)
+#         y_E, y_I = trim_data(y_E, y_I)
+#         save_data(y_E, y_I, J_array, P_array, w_array)
+#         count += 1
+#         print(count)
 
-if __name__ == "__main__":
-    main(dataset_size=10000)
+# if __name__ == "__main__":
+#     main(dataset_size=10000)
---
+++ v1_artificial_network_larger_image_size.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import pickle
+from utils.rodents_routine import get_device
+
+class V1CNN(nn.Module):
+    def __init__(self, num_classes=12):
+        super(V1CNN, self).__init__()
+        self.features_excit = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1), # (21 x 18 x 1) -> (21 x 18 x 4)
+            nn.ReLU(),
+            nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1), # (21 x 18 x 4) -> (21 x 18 x 8)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2), # (21 x 18 x 8) -> (10 x 9 x 8)
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1), # (10 x 9 x 8) -> (10 x 9 x 16)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) # (10 x 9 x 16) -> (5 x 4 x 16)
+        )
+
+        self.features_inhib = nn.Sequential(
+            nn.Conv2d(1, 4, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2) # -> (5 x 4 x 16)
+        )
+
+
+        self.regressor = nn.Sequential(
+            nn.Linear(5 * 4 * 16 * 2, 64),  # nuber of images * number of output channel * image width * image height
+            nn.ReLU(),
+            nn.Linear(64, 12),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            nn.Linear(12, num_classes),  # 12 classes
+            nn.Sigmoid()
+        )
+
+
+    def forward(self, x):  # input is a len 2 list of torch vector of size (batch_size, 48 (or 12), 21, 18)
+
+        h_E = torch.zeros((len(x), 320), device=x[0].device)
+        count = 0
+        for image in x[0].permute(1, 0, 2, 3):
+            count += 1
+            image: torch.Tensor = image.unsqueeze(1)
+            image = self.features_excit(image)
+            image = torch.flatten(image, 1)
+            h_E = h_E + image
+        h_E = h_E / count
+
+        h_I = torch.zeros((len(x), 320), device=x[0].device)
+        count = 0
+        for image in x[1].permute(1, 0, 2, 3):
+            count += 1
+            image: torch.Tensor = image.unsqueeze(1)
+            image = self.features_inhib(image)
+            image = torch.flatten(image, 1)
+            h_I = h_I + image
+        h_I = h_I / count
+        
+        h = torch.concatenate([h_E, h_I], dim=1)
+        h = self.regressor(h)
+        
+        return h
+
+
+class V1Dataset(Dataset):
+    def __init__(self, pickle_file_content, device="cpu", data_type="train"):
+        self.input = pickle_file_content[f"X_{data_type}"]
+        self.ground_truth_params = pickle_file_content[f"y_{data_type}"]
+        self.max_E = pickle_file_content["max_E"]
+        self.max_I = pickle_file_content["max_I"]
+        self.size = len(pickle_file_content[f"X_{data_type}"])
+        self.device = device
+
+    def __len__(self):
+        return self.size
+    
+
+    def __getitem__(self, index):
+        sample = {
+            'input': None,
+            'target': None
+        }
+
+        sample["target"] = self.ground_truth_params[index].to(self.device)
+        sample["input"] = [(self.input[index][0] / self.max_E).to(self.device),
+                           (self.input[index][1] / self.max_I).to(self.device)]
+        return sample
+    
+
+def calc_theoretical_weights_tot(J, P, w, N_b, device="cpu"):
+    """Calculate weights tot for the contraints"""
+    k = 1 / (4 * w * torch.pi / 180) ** 2
+    bessel: torch.Tensor = torch.special.i0(k)
+    return J * torch.sqrt(torch.tensor(N_b, device=device)) * P * torch.exp(-k) * bessel
+
+
+def validate_weight_matrix(model_output, device="cpu"):
+    W_tot_EE = calc_theoretical_weights_tot(model_output[:,0] * 100, model_output[:,4], model_output[:,8] * 180, 800, device=device)
+    W_tot_EI = calc_theoretical_weights_tot(model_output[:,1] * 100, model_output[:,5], model_output[:,9] * 180, 200, device=device)
+    W_tot_IE = calc_theoretical_weights_tot(model_output[:,2] * 100, model_output[:,6], model_output[:,10] * 180, 800, device=device)
+    W_tot_II = calc_theoretical_weights_tot(model_output[:,3] * 100, model_output[:,7], model_output[:,11] * 180, 200, device=device)
+    
+    first_condition = torch.maximum((W_tot_EE / W_tot_IE) - 1, torch.tensor(0, device=device))
+    second_condition = torch.maximum((W_tot_EI / W_tot_II) - 1, torch.tensor(0, device=device))
+    return torch.mean(torch.maximum(first_condition, second_condition))
+
+if __name__ == "__main__":
+    device = get_device("cuda:0")
+    PATH = "V1CNN.pth"
+
+    with open("dataset_full_for_training.pkl", 'rb') as f:
+        pickle_data = pickle.load(f)
+
+    batch_size = 8
+    shuffle = True
+    train_dataset = V1Dataset(pickle_data, data_type="train", device=device)
+    test_dataset = V1Dataset(pickle_data, data_type="test", device=device)
+    val_dataset = V1Dataset(pickle_data, data_type="val", device=device)
+
+    train_data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+    test_data_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    val_data_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    model = V1CNN().to(device)
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    num_epochs = 100
+    current_val_loss = 1000000
+    patience = 2
+
+    model.eval()
+    running_loss = 0.0
+    for batch in test_data_loader:
+        inputs = batch["input"]
+        targets = batch['target']
+        outputs = model.forward(inputs)
+        loss = criterion(outputs, targets)
+        running_loss += loss.item()
+
+    print(f'\ntesting Loss: {running_loss / len(val_data_loader):.4f}\n')
+
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        running_total_loss = 0.0
+        for batch in train_data_loader:
+            inputs = batch["input"]
+            targets = batch['target']  # just the first value
+            
+            optimizer.zero_grad()
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, targets)  # need to add the bessel val and weights normalisation layer
+            # bessel = validate_weight_matrix(outputs, device=device)
+            # total_loss = loss + bessel * 0.1  # for weighting  # Bessel val is NaN
+            # total_loss.backward()
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+            # running_total_loss += total_loss.item()
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Training MSE Loss: {running_loss / len(train_data_loader):.4f}')
+        # print(f'Epoch [{epoch + 1}/{num_epochs}], Training Total Loss: {running_total_loss / len(train_data_loader):.4f}')
+
+        # Get validation losses
+        model.eval()
+        running_loss = 0.0
+        for batch in val_data_loader:
+            inputs = batch["input"]
+            targets = batch['target']
+            outputs = model.forward(inputs)
+            loss = criterion(outputs, targets)
+            running_loss += loss.item()
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Eval MSE Loss: {running_loss / len(val_data_loader):.4f}\n')
+        if current_val_loss < running_loss / len(val_data_loader) and epoch > 20:
+            patience -= 1
+        else:
+            patience = 2
+            torch.save(model.state_dict(), PATH)
+        
+        if patience == 0:
+            print("Early stopping")
+            break
+
+        current_val_loss = running_loss / len(val_data_loader)
+
+    model.eval()
+    running_loss = 0.0
+    for batch in test_data_loader:
+        inputs = batch["input"]
+        targets = batch['target']
+        outputs = model.forward(inputs)
+        loss = criterion(outputs, targets)
+        running_loss += loss.item()
+
+    print(f'testing Loss: {running_loss / len(val_data_loader):.4f}\n')
+    print(f"last test output: {outputs[-1]}")
+    print(f"last test target: {targets[-1]}")
+    print(f"last test output: {outputs[0]}")
+    print(f"last test target: {targets[0]}")
