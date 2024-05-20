2b2c1caf7dec6edc690e57b80d25f9a1689a0f55
paopaoch <paopaochofficial@gmail.com>
2024-05-20 02:20:47 +0100

    update method val xNES

--- method_validation_nes.py
+++ method_validation_nes.py
@@ -12,11 +12,11 @@ if __name__ == "__main__":
     dir_name = f"method_validation_{datetime.now()}"
     create_directory_if_not_exists(dir_name)
 
-    desc = "NES with JSD"
+    desc = "NES with MMD"
 
     device = get_device("cuda:1")
 
-    n = 1000
+    n = 10000
 
     executer = NetworkExecuterWithSimplifiedFF(n, device=device, scaling_g=0.15)
 
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     J_array = [-0.9308613398652443, -2.0604571635972393, -0.30535063458645906, -1.802886963254238]
     P_array = [-1.493925025312256, 1.09861228866811, -1.493925025312256, 1.09861228866811]
     w_array = [-1.5314763709643886, -1.5314763709643886, -1.5314763709643886, -1.5314763709643886] 
-    heter_ff = torch.tensor([0.2], device=device)
+    heter_ff = torch.tensor([-1.3862943611198906], device=device)
 
     J_array = torch.tensor(J_array, device=device)
     P_array = torch.tensor(P_array, device=device)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     W = wg.generate_weight_matrix()
     executer.update_heter_ff(heter_ff)
     tuning_curves, avg_step = executer.run_all_orientation_and_contrast(W)
-    y_E, y_I = tuning_curves[:800], tuning_curves[800:]
+    y_E, y_I = tuning_curves[:8000], tuning_curves[8000:]
 
 
     var_list = [0.5, 0.5, 0.5, 0.5,
@@ -43,10 +43,8 @@ if __name__ == "__main__":
 
     for _ in range(11):
         file_name = f"{dir_name}/log_nes_run_{datetime.now()}"
-        mean_array = np.random.rand(12) * 9 - 4.5
+        mean_array = np.random.rand(13) * 9 - 4.5
         mean_list: list = mean_array.tolist()
-        heter_ff_rand = np.random.rand(1)[0]
-        mean_list.append(heter_ff)
         mean, cov = make_torch_params(mean_list, var_list, device=device)
 
-        nes_multigaussian_optim(mean, cov, 200, 24, y_E, y_I, device=device, neuron_num=n, desc=desc, trials=1, alpha=0.1, eta_delta=1, avg_step_weighting=0.1, stopping_criterion_step=0.0000001, adaptive_lr=False, file_name=file_name)
+        nes_multigaussian_optim(mean, cov, 200, 13, y_E, y_I, device=device, neuron_num=n, desc=desc, trials=1, alpha=0.1, eta_delta=1, avg_step_weighting=0.1, stopping_criterion_step=0.0000001, adaptive_lr=False, file_name=file_name)
